5a664b796cae9862fe660fa774f031834ad521d4
Avvai Aravind <avvaiaravind@gmail.com>
2025-02-12 18:26:10 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-grow items-center justify-center">
+      <p className="bg-blue-600 p-4 text-center text-5xl text-white">
+        Vite Template
+      </p>
+    </div>
+  );
+}
+
+export default App;
