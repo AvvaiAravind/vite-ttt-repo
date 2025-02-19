8b572582ad12a4a8c64f69d58eec492b16a893b2
AvvaiAravind <160732760+AvvaiAravind@users.noreply.github.com>
2025-02-19 20:55:50 +0530

    Update vite.config.ts added import path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path"
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
