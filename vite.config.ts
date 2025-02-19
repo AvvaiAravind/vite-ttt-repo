681d3628f46c49bb3b6263c004292f465f676515
AvvaiAravind <160732760+AvvaiAravind@users.noreply.github.com>
2025-02-19 19:52:55 +0530

    Update vite.config.ts for alias path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  // alias path
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/*"),
+    },
+  },
 });
