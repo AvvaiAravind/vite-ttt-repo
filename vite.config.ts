a795d5306200f0a34675e91fa9d66397c66e8953
Avvai Aravind <avvaiaravind@gmail.com>
2025-04-08 21:20:52 +0530

    modified alias paths & added prettierignore

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   // alias path
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src/*"),
+      "@/src": path.resolve(__dirname, "./src"),
     },
   },
 });
