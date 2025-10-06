976a2d29177beee2dfb621ce8032f2c4a28ce86d
Avvai Aravind <avvaiaravind@gmail.com>
2025-10-06 07:07:46 +0530

    feat: add shadcn

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,49 @@
 import js from "@eslint/js";
-import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
+  { ignores: ["dist", "node_modules", "**/*.config.js", "**/*.config.ts"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      // manual rules'
-      "no-console": ["warn", { allow: ["warn", "error"] }],
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
-  }
-);
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+
+      // Disabled for shadcn/ui components
+      "react-refresh/only-export-components": "off",
+
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from "path"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  // build
+  build: {
+    sourcemap: true, // Helpful for debugging production
+  },
+
+  //server
+  server: {
+    port: 5173, // Optional: set custom port
+    open: true, // Optional: auto-open browser
+  },
+
   // alias path
   resolve: {
     alias: {
-      "@/src": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
